6c85fbbcd6551c057187a34730d0eac5edac613c
alecarejola <aarejola@gbox.adnu.edu.ph>
2021-07-07 19:49:43 +0800

    forms, routings, model and view added, bootstrap

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #app/welcome_controller
+  #always route to welcome/index page. this will be the home page
+  root to: "welcome#index"  
+  get "welcome/index"
+
+  #this will be the register page route
+  get "register", to: "register#register"
+
+  #this will be the login page route
+  get "login", to: "login#login"
+
+  #this will be the service seeker page
+  get "services", to: "service#service"
+
+end
